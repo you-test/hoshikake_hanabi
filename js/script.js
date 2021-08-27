2aa43160584b6d0e2d9bb08a9e272b01c12f49eb
yusuke <sato11.jp@gmail.com>
2021-08-28 00:07:59 +0900

    responsive & js finished

---
+++ js/script.js
@@ -0,0 +1,48 @@
+$(function() {
+
+  /* mainvisual slick */ 
+  $('.mv-image').slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+    arrows: false,
+    dots: false,
+    slidesToshow: 1,
+    slidesToScroll: 1,
+    fade: true,
+    speed: 1800,
+  });
+
+  /* berger menu */ 
+  $('.berger-btn').click(function() {
+    $(this).toggleClass('open');
+    $('nav').toggleClass('open');
+  });
+
+  /* scroll fade in */ 
+  $(window).scroll(function() {
+    $('.fadein').each(function() {
+      let scroll = $(window).scrollTop();
+      let target = $(this).offset().top;
+      let windowHeight = $(window).height();
+
+      if (scroll > target - windowHeight + 150) {
+        $(this).addClass('appearance');
+      }
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
